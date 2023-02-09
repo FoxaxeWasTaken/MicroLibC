74bc74cd835d5ba7e73566b236d8c6a58e6bade7
foxaxe <axel.idoux@epitech.eu>
2023-02-09 13:58:29 +0100

    feat: adding the implementation of memset into the asm shared lib

---
+++ memset.asm
@@ -0,0 +1,20 @@
+bits 64
+section .text
+global memset
+
+memset:
+    enter 0, 0
+    mov rax, rdi
+    xor rcx, rcx
+    jmp .loop
+
+.loop:
+    cmp rcx, rdx
+    jz .end
+    mov [rax + rcx], sil
+    inc rcx
+    jmp .loop
+
+.end:
+    leave
+    ret
